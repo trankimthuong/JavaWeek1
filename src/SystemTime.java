08b5d75913dcf84cb2e9769fe174ade2c92be665
= <=>
2019-08-29 08:30:02 +0700

    demo system time

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class SystemTime {
     public static void main(String[] args) {
         Date now = new Date();
-        System.out.println("now is: " + now.getTime());
+        System.out.println("now is: " + now);
     }
 }
